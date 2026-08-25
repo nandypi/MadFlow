326c1e2ab8196e6ecb28b4cbe68c00e61f7b7c9a
Nandy.py <parnapalli2004@gmail.com>
2026-08-25 15:11:04 +0530

    updated bat to avoid security block by windows

--- Viva API.bat
+++ Viva API.bat
@@ -4,8 +4,5 @@ REM This batch file is used to start the FastAPI server for the VivaFlow project
 
 cd /d "C:\Users\nandy\Documents\Code\VivaFlow"
 
-REM Activate the virtual environment
-call ".venv\Scripts\activate.bat"
-
 REM Start the FastAPI server
-uvicorn app:app --host 127.0.0.1 --port 8765
+.venv\Scripts\python.exe -m uvicorn app:app --host 127.0.0.1 --port 8765
